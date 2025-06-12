d6a3518e02f4bf7a70a4f95e4dafb5e1acdaabd6
jing <chenyichun19971110@gmail.com>
2025-06-12 03:18:08 -0700

    building header

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hello World</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
